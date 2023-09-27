02b29201afe44a302dfdaa1b9b8eb08bc4043892
Ali Yousefi <ali.visual.studio@gmail.com>
2023-09-27 17:45:24 +0330

    Rename and clear some method names

--- src/CSharp/EasyMicroservices.OrderingMicroservice.Domain/Contracts/Common/OrderLanguageContract.cs
+++ src/CSharp/EasyMicroservices.OrderingMicroservice.Domain/Contracts/Common/OrderLanguageContract.cs
@@ -3,9 +3,6 @@ using EasyMicroservices.Cores.Interfaces;
 using EasyMicroservices.Domain.DataTypes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasyMicroservices.OrderingMicroservice.Contracts.Common
 {
