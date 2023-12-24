e5ec22d5c722f3423f0fbd8189a25ee3e0e7830b
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-24 18:24:57 +0330

    Refactor and use new core stuff

--- src/CSharp/EasyMicroservices.OrderingMicroservice.WebApi/Controllers/CountingUnitController.cs
+++ src/CSharp/EasyMicroservices.OrderingMicroservice.WebApi/Controllers/CountingUnitController.cs
@@ -1,88 +1,14 @@
-﻿using EasyMicroservices.ContentsMicroservice.Clients.Helpers;
-using EasyMicroservices.Cores.AspCoreApi;
+﻿using EasyMicroservices.Cores.AspCoreApi;
 using EasyMicroservices.Cores.AspEntityFrameworkCoreApi.Interfaces;
-using EasyMicroservices.Cores.Contracts.Requests;
 using EasyMicroservices.OrderingMicroservice.Contracts.Common;
 using EasyMicroservices.OrderingMicroservice.Contracts.Requests;
 using EasyMicroservices.OrderingMicroservice.Database.Entities;
-using EasyMicroservices.ServiceContracts;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
-namespace EasyMicroservices.OrderingMicroservice.WebApi.Controllers
+namespace EasyMicroservices.OrderingMicroservice.WebApi.Controllers;
+
+public class CountingUnitController : MultilingualSimpleQueryServiceController<CountingUnitEntity, CreateCountingUnitRequestContract, UpdateCountingUnitRequestContract, CountingUnitContract, CountingUnitLanguageContract, long>
 {
-    public class CountingUnitController : SimpleQueryServiceController<CountingUnitEntity, CreateCountingUnitRequestContract, UpdateCountingUnitRequestContract, CountingUnitContract, long>
+    public CountingUnitController(IUnitOfWork unitOfWork) : base(unitOfWork)
     {
-        private readonly IConfiguration _config;
-        private readonly ContentLanguageHelper _contentHelper;
-        readonly IUnitOfWork _unitOfWork;
-        public CountingUnitController(IUnitOfWork unitOfWork, IConfiguration config) : base(unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-            _config = config;
-            _contentHelper = new(new Contents.GeneratedServices.ContentClient(_config.GetValue<string>("RootAddresses:Content"), new HttpClient()));
-        }
-
-        public override async Task<MessageContract<long>> Add(CreateCountingUnitRequestContract request, CancellationToken cancellationToken = default)
-        {
-            var result = await base.Add(request, cancellationToken);
-            if (result)
-            {
-                var addedItem = await GetById(new Cores.Contracts.Requests.GetIdRequestContract<long> { Id = result.Result })
-                    .AsCheckedResult();
-                request.UniqueIdentity = addedItem.UniqueIdentity;
-                await _contentHelper.AddToContentLanguage(request)
-                    .AsCheckedResult();
-            }
-            return result;
-        }
-        public override async Task<ListMessageContract<CountingUnitContract>> Filter(FilterRequestContract filterRequest, CancellationToken cancellationToken = default)
-        {
-            var result = await base.Filter(filterRequest, cancellationToken);
-            result.ThrowsIfFails();
-            await _contentHelper.ResolveContentLanguage(result.Result, filterRequest.Language);
-            return result;
-        }
-
-
-        public override async Task<MessageContract<CountingUnitContract>> Update(UpdateCountingUnitRequestContract request, CancellationToken cancellationToken = default)
-        {
-            var result = await base.Update(request, cancellationToken).AsCheckedResult();
-            request.UniqueIdentity = result.UniqueIdentity;
-            await _contentHelper.UpdateToContentLanguage(request).AsCheckedResult();
-            return result;
-        }
-
-        public override async Task<MessageContract<CountingUnitContract>> UpdateChangedValuesOnly(UpdateCountingUnitRequestContract request, CancellationToken cancellationToken = default)
-        {
-            var result = await base.UpdateChangedValuesOnly(request, cancellationToken).AsCheckedResult();
-            request.UniqueIdentity = result.UniqueIdentity;
-            await _contentHelper.UpdateToContentLanguage(request).AsCheckedResult();
-            return result;
-        }
-
-        [HttpPost]
-        public async Task<ListMessageContract<CountingUnitContract>> GetAllByLanguage(GetByLanguageRequestContract request, CancellationToken cancellationToken = default)
-        {
-            var result = await base.GetAll(cancellationToken);
-            if (result)
-            {
-                await _contentHelper.ResolveContentLanguage(result.Result, request.Language);
-            }
-            return result;
-        }
-
-        [HttpPost]
-        public async Task<MessageContract<CountingUnitLanguageContract>> GetByIdAllLanguage(GetIdRequestContract<long> request, CancellationToken cancellationToken = default)
-        {
-            var result = await base.GetById(request.Id, cancellationToken);
-            if (result)
-            {
-                var mapped = _unitOfWork.GetMapper().Map<CountingUnitLanguageContract>(result.Result);
-                await _contentHelper.ResolveContentAllLanguage(mapped);
-                return mapped;
-            }
-            return result.ToContract<CountingUnitLanguageContract>();
-        }
     }
 }
